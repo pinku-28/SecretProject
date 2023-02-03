5b4291da53d0bddb8565bc63b8e0e0fd8831078d
pinkuu <penasjp28@gmail.com>
2023-02-03 09:29:38 +0800

    main functions done
    main functions include:
    -moving No button
    -forceNo button
    -all buttons removed post-button press
    
    next goals;
    -add floating/fading hearts when pressing background
    -idk maybe a result ticket

---
+++ script.js
@@ -0,0 +1,42 @@
+const wrapper = document.querySelector('.wrapper');
+const title = document.querySelector('.title');
+const subtext = document.querySelector('.note');
+const buttonYes = document.querySelector('#buttonYes');
+const buttonNo = document.querySelector('#buttonNo');
+const header = document.querySelector('.header');
+const forceNo = document.querySelector('.forceNo');
+
+const wrapperRect = wrapper.getBoundingClientRect();
+const buttonNoRect = buttonNo.getBoundingClientRect();
+
+function clickYes(){
+    title.innerHTML = "YAY!";
+    subtext.innerHTML = "you may request a movie (≧∇≦)ﾉ";
+    removeButtons();
+}
+
+function hoverNo(){
+    const i = Math.floor(Math.random() * (wrapperRect.width
+        - buttonNoRect.width)) + 1;
+    const j = Math.floor(Math.random() * (wrapperRect.height
+        - buttonNoRect.height)) + 1;
+
+    buttonNo.style.left = i + "px";
+    buttonNo.style.top = j + "px";
+}
+
+function clickNo(){
+    title.innerHTML = ":<";
+    subtext.innerHTML = "awww okay 😉"
+    removeButtons();
+}
+
+function removeButtons(){;
+    forceNo.remove();
+    buttonNo.remove();
+    buttonYes.remove();
+}
+
+
+//for No, kailangan ko ng function na
+//magrarandomize ng position ng No everytime it is hovered
